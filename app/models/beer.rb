e6569026aedd739e459ef1cc3ecdfa72a292bbba
Frans Ojala <frans.ojala@helsinki.fi>
2014-01-19 11:55:13 +0200

    Tehtävät 1-4 sekä panimon nimen näyttäminen id:n sijaan.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,15 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    if ratings.count>0
+      sum = 0
+      ratings.each do |rating|
+        sum += rating.score
+      end
+      sum/ratings.count
+    end
+  end
+
 end
