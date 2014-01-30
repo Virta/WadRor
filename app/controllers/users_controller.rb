f0060b5933b6f56b4eb4d04b59e61419b31564a1
Frans Ojala <frans.ojala@helsinki.fi>
2014-01-30 11:24:50 +0200

    tehtävä 12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,11 +60,12 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy if current_user == @user
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+    if current_user == @user
+      @user.destroy
+      session[:user_id] = nil
+      redirect_to :root
     end
+
   end
 
   private
