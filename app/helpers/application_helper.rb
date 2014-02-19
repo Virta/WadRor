d2cbf23211dfe57819668460542bead11e80d7e4
Frans Ojala <frans.ojala@helsinki.fi>
2014-02-19 19:10:06 +0200

    viikon 6 tehtävään 11

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def edit_and_destroy_buttons(item)
+    unless current_user.nil? and current_user.is_admin?
+      notice = "Admin:"
+      edit = link_to('Edit', url_for([:edit, item]), class:"btn btn-primary")
+      del = link_to('Destroy', item, method: :delete,
+                    data: {confirm: 'Are you sure?' }, class:"btn btn-danger")
+      raw("#{notice} #{edit} #{del}")
+    end
+  end
+  
+  def round(param)
+    number_with_precision param, precision: 3, significant: true, strip_insignificant_zeros: true
+  end
 end
